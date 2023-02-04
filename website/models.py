c0afbbb9947d0a3211d7a68c8a3f019674a0ce78
Harish <sabarinathan.project@gmail.com>
2023-02-04 12:54:14 +0530

    database &  login

--- website/models.py
+++ website/models.py
@@ -3,16 +3,22 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
+class register(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    name=db.Column(db.String(100))
+    email = db.Column(db.String(150), unique=True)
+    rollno=db.Column(db.String(100))
+    dept=db.Column(db.String(100))
+    
+    IEEE=db.Column(db.String(10))
+    
+    event=db.Column(db.String(100))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    username = db.Column(db.String(150))
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
-    notes = db.relationship('Note')
+   
